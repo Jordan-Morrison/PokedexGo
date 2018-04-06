eb87018a93b324e3f333ea31a44e8d4638e12fc5
Jordan Morrison <32398038+Jordan-Morrison@users.noreply.github.com>
2018-04-05 20:13:32 -0400

    Updating for Gen 4 Preview

--- native app/platforms/android/app/build/intermediates/assets/debug/www/cordova_plugins.js
+++ native app/platforms/android/app/build/intermediates/assets/debug/www/cordova_plugins.js
@@ -8,21 +8,12 @@ module.exports = [
       "analytics",
       "ga"
     ]
-  },
-  {
-    "id": "cordova-plugin-splashscreen.SplashScreen",
-    "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
-    "pluginId": "cordova-plugin-splashscreen",
-    "clobbers": [
-      "navigator.splashscreen"
-    ]
   }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
   "cordova-plugin-google-analytics": "1.8.3",
-  "cordova-plugin-splashscreen": "5.0.1",
   "cordova-plugin-whitelist": "1.3.3"
 };
 // BOTTOM OF METADATA
